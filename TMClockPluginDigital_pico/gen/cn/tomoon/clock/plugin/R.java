eff5710c402a4ca27f2bf007e90673315953d5b2
Zhang Zhiwei <lz@tm.com>
2013-10-15 16:22:44 +0800

    update

--- TMClockPluginDigital_pico/gen/cn/tomoon/clock/plugin/R.java
+++ TMClockPluginDigital_pico/gen/cn/tomoon/clock/plugin/R.java
@@ -11,10 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class bool {
-        public static final int allow_rotation=0x7f060003;
-        public static final int config_hardwareAccelerated=0x7f060000;
-        public static final int config_largeHeap=0x7f060001;
-        public static final int is_large_screen=0x7f060002;
+        public static final int allow_rotation=0x7f040003;
+        public static final int config_hardwareAccelerated=0x7f040000;
+        public static final int config_largeHeap=0x7f040001;
+        public static final int is_large_screen=0x7f040002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -23,52 +23,48 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f080000;
-        public static final int activity_vertical_margin=0x7f080001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int default_bg=0x7f020022;
         public static final int dialog=0x7f020000;
         public static final int dialog_divider_horizontal_light=0x7f020001;
         public static final int divider_vertical_dark=0x7f020002;
-        public static final int face_layer=0x7f020003;
-        public static final int green0=0x7f020004;
-        public static final int green1=0x7f020005;
-        public static final int green2=0x7f020006;
-        public static final int green3=0x7f020007;
-        public static final int green4=0x7f020008;
-        public static final int green5=0x7f020009;
-        public static final int green6=0x7f02000a;
-        public static final int green7=0x7f02000b;
-        public static final int green8=0x7f02000c;
-        public static final int green9=0x7f02000d;
-        public static final int green_dark_8=0x7f02000e;
-        public static final int greenam=0x7f02000f;
-        public static final int greenam_dark=0x7f020010;
-        public static final int greendot=0x7f020011;
-        public static final int greenpm=0x7f020012;
-        public static final int greenpm_dark=0x7f020013;
-        public static final int ic_launcher=0x7f020014;
-        public static final int small_green0=0x7f020015;
-        public static final int small_green1=0x7f020016;
-        public static final int small_green2=0x7f020017;
-        public static final int small_green3=0x7f020018;
-        public static final int small_green4=0x7f020019;
-        public static final int small_green5=0x7f02001a;
-        public static final int small_green6=0x7f02001b;
-        public static final int small_green7=0x7f02001c;
-        public static final int small_green8=0x7f02001d;
-        public static final int small_green9=0x7f02001e;
-        public static final int small_green_dark_8=0x7f02001f;
-        public static final int small_greendot=0x7f020020;
-        public static final int watch_face=0x7f020021;
+        public static final int green0=0x7f020003;
+        public static final int green1=0x7f020004;
+        public static final int green2=0x7f020005;
+        public static final int green3=0x7f020006;
+        public static final int green4=0x7f020007;
+        public static final int green5=0x7f020008;
+        public static final int green6=0x7f020009;
+        public static final int green7=0x7f02000a;
+        public static final int green8=0x7f02000b;
+        public static final int green9=0x7f02000c;
+        public static final int green_dark_8=0x7f02000d;
+        public static final int greenam=0x7f02000e;
+        public static final int greenam_dark=0x7f02000f;
+        public static final int greendot=0x7f020010;
+        public static final int greenpm=0x7f020011;
+        public static final int greenpm_dark=0x7f020012;
+        public static final int ic_launcher=0x7f020013;
+        public static final int small_green0=0x7f020014;
+        public static final int small_green1=0x7f020015;
+        public static final int small_green2=0x7f020016;
+        public static final int small_green3=0x7f020017;
+        public static final int small_green4=0x7f020018;
+        public static final int small_green5=0x7f020019;
+        public static final int small_green6=0x7f02001a;
+        public static final int small_green7=0x7f02001b;
+        public static final int small_green8=0x7f02001c;
+        public static final int small_green9=0x7f02001d;
+        public static final int small_green_dark_8=0x7f02001e;
+        public static final int small_greendot=0x7f02001f;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a000e;
+        public static final int action_settings=0x7f0a000d;
         public static final int am=0x7f0a0003;
         public static final int am_pm=0x7f0a0002;
         public static final int bigdot=0x7f0a0007;
-        public static final int face=0x7f0a000d;
         public static final int hour1=0x7f0a0005;
         public static final int hour2=0x7f0a0006;
         public static final int minute1=0x7f0a0008;
@@ -81,7 +77,7 @@ public final class R {
         public static final int timeLayout=0x7f0a0001;
     }
     public static final class integer {
-        public static final int config_digtal_scale=0x7f070000;
+        public static final int config_digtal_scale=0x7f050000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -94,12 +90,12 @@ public final class R {
         public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int dot_str=0x7f040004;
-        public static final int hello_world=0x7f040002;
-        public static final int hour_str=0x7f040003;
-        public static final int min_str=0x7f040005;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int dot_str=0x7f070004;
+        public static final int hello_world=0x7f070002;
+        public static final int hour_str=0x7f070003;
+        public static final int min_str=0x7f070005;
     }
     public static final class style {
         /** 
@@ -122,33 +118,11 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f080001;
     }
 }
